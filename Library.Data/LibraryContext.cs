358b8ba657926fd67ab9a7970dc2af41d5e41e75
Nguyen <gammalux09821@gmail.com>
2017-08-06 08:47:21 +0700

    add connection string and database context to generate the table in database

---
+++ Library.Data/LibraryContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Data.Models;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.EntityFrameworkCore;
+using Library.Data;
 namespace Library
 {
     public class Startup
@@ -29,6 +26,8 @@ namespace Library
         {
             // Add framework services.
             services.AddMvc();
+            services.AddDbContext<LibraryContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
